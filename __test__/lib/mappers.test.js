69c3fff597ab8e07e9c966ac098ffd68972aff9e
Chris Meyers <cm.02.93@gmail.com>
2020-11-02 20:47:16 -0700

    added more tests

---
+++ __test__/lib/mappers.test.js
@@ -0,0 +1,35 @@
+const yup = require('yup');
+const validators = require('../../lib/validators');
+const mappers = require('../../lib/mappers');
+
+describe('new mapper translation', () => {
+  test('if it fails schema validation', async () => {
+    const input = { apples: true };
+    const validator = validators.old;
+
+    await expect(mappers.new(input, validator)).rejects.toThrow();
+  });
+
+  test('if it passes schema validation', async () => {
+    const input = {
+      weird_company_name: 'test',
+      reversed_flag: false,
+    };
+    const validator = validators.old;
+
+    await expect(mappers.new(input, validator)).resolves;
+  });
+
+  test('if it returns the expected values', async () => {
+    const input = {
+      weird_company_name: 'tset',
+      reversed_flag: false,
+    };
+    const validator = validators.old;
+
+    await expect(mappers.new(input, validator)).resolves.toEqual({
+      company_name: 'test',
+      flag: true,
+    });
+  });
+});
---
+++ __test__/lib/resolvers.test.js
@@ -0,0 +1,29 @@
+const { resolveValidator, resolveMapper } = require('../../lib/resolvers');
+
+describe('validator resolution', () => {
+  test('if it prevents loading an invalid validator', () => {
+    expect(() => {
+      resolveValidator('doesNotExist');
+    }).toThrow();
+  });
+
+  test('if it loads a validator that exists', () => {
+    expect(() => {
+      resolveValidator('old');
+    }).not.toThrow();
+  });
+});
+
+describe('mapper resolution', () => {
+  test('if it prevents loading an invalid mapper', () => {
+    expect(() => {
+      resolveMapper('doesNotExist');
+    }).toThrow();
+  });
+
+  test('if it loads a mapper that exists', () => {
+    expect(() => {
+      resolveMapper('new');
+    }).not.toThrow();
+  });
+});
