42e433e8982c60778e422f0359252ea49202f934
Chris Meyers <cm.02.93@gmail.com>
2020-11-02 21:04:50 -0700

    moved example code out of index.js

---
+++ examples/simple.js
@@ -0,0 +1,24 @@
+const dataMapper = require('..');
+
+const convert = dataMapper.convert;
+const { resolveValidator, resolveMapper } = dataMapper.resolvers;
+
+const input = {
+  weird_company_name: '321 gnitset',
+  reversed_flag: false,
+};
+
+try {
+  const map = convert(resolveValidator('old'))(resolveMapper('new'));
+
+  (async () => {
+    try {
+      const result = await map(input);
+      console.log(result);
+    } catch (error) {
+      console.error(error.errors);
+    }
+  })();
+} catch (error) {
+  console.error(error.message);
+}
--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 const convert = require('./lib/convert');
-const { resolveValidator, resolveMapper } = require('./lib/resolvers');
+const resolvers = require('./lib/resolvers');
 
-const input = {
-  weird_company_name: '321 gnitset',
-  reversed_flag: false,
-};
-
-try {
-  const map = convert(resolveValidator('old'))(resolveMapper('new'));
-
-  (async () => {
-    try {
-      const result = await map(input);
-      console.log(result);
-    } catch (error) {
-      console.error(error.errors);
-    }
-  })();
-} catch (error) {
-  console.error(error.message);
-}
+exports.convert = convert;
+exports.resolvers = resolvers;
