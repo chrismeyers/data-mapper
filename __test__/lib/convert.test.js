ffa97064522fc83c165960d8ac8ab37dcc635a41
Chris Meyers <cm.02.93@gmail.com>
2020-11-15 18:02:21 -0700

    improved convert tests, removed deprecated tests

--- __test__/lib/convert.test.js
+++ __test__/lib/convert.test.js
@@ -1,74 +1,35 @@
-/* eslint-disable no-unused-vars */
 const yup = require('yup');
 const convert = require('../../lib/convert');
 
-describe('from and to validation', () => {
-  test('if it prevents invalid from value', () => {
-    const resolveValidator = (type) => {
-      throw new Error('undefined validator');
-    };
-
-    expect(() => {
-      convert(resolveValidator('doesNotExist'));
-    }).toThrow();
-  });
-
-  test('if it allows correct from value', () => {
-    const resolveValidator = (type) => {};
-
-    expect(() => {
-      convert(resolveValidator('exists'));
-    }).not.toThrow();
-  });
-
-  test('if it prevents invalid to value', () => {
-    const resolveValidator = (type) => {};
-    const resolveMapper = (type) => {
-      throw new Error('undefined validator');
-    };
-
-    expect(() => {
-      convert(resolveValidator('exists'))(resolveMapper('doesNotExist'));
-    }).toThrow();
-  });
-
-  test('if it allows correct to value', () => {
-    const resolveValidator = (type) => {};
-    const resolveMapper = (type) => {};
-
-    expect(() => {
-      convert(resolveValidator('exists'))(resolveMapper('exists'));
-    }).not.toThrow();
-  });
-});
-
-describe('mapping validation', () => {
+describe('transformation pipeline', () => {
   test('if it prevents converting invalid data shapes', async () => {
     const input = { oranges: true };
 
-    const resolveValidator = (type) => {
-      return yup.object().shape({
-        apples: yup.boolean().required(),
-      });
-    };
-    const resolveMapper = async (type) => {};
+    const customValidator = yup.object().shape({
+      apples: yup.boolean().required(),
+    });
 
-    const map = convert(resolveValidator('exists'))(resolveMapper('exists'));
+    const customMapper = async (data, validator) => {
+      await validator.validate(data);
+    };
+
+    const map = convert(customValidator)(customMapper);
 
     await expect(map(input)).rejects.toThrow();
   });
 
-  test('if it converts data with the correct shape', async () => {
+  test('if it proceeds converting data with the correct shape', async () => {
     const input = { apples: true };
 
-    const resolveValidator = (type) => {
-      return yup.object().shape({
-        apples: yup.boolean().required(),
-      });
-    };
-    const resolveMapper = (type) => async (data, validator) => {};
+    const customValidator = yup.object().shape({
+      apples: yup.boolean().required(),
+    });
 
-    const map = convert(resolveValidator('exists'))(resolveMapper('exists'));
+    const customMapper = async (data, validator) => {
+      await validator.validate(data);
+    };
+
+    const map = convert(customValidator)(customMapper);
 
     await expect(map(input)).resolves;
   });
@@ -76,19 +37,22 @@ describe('mapping validation', () => {
   test('if it converts data correctly', async () => {
     const input = { apples: true };
 
-    const resolveValidator = (type) => {
-      return yup.object().shape({
-        apples: yup.boolean().required(),
-      });
-    };
-    const resolveMapper = (type) => async (data, validator) => {
-      return { not_apples: false };
+    const customValidator = yup.object().shape({
+      apples: yup.boolean().required(),
+    });
+
+    const customMapper = async (data, validator) => {
+      const valid = await validator.validate(data);
+
+      return {
+        apples: !valid.apples,
+      };
     };
 
-    const map = convert(resolveValidator('exists'))(resolveMapper('exists'));
+    const map = convert(customValidator)(customMapper);
 
     await expect(map(input)).resolves.toEqual({
-      not_apples: false,
+      apples: false,
     });
   });
 });
